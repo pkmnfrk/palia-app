096f00d95139b6aabfdd0e4efc6b3d2f2f954810
Mike Caron <caron.mike@outlook.com>
2023-09-02 17:05:50 -0400

    add support for overlays on items

--- web/src/components/Item.js
+++ web/src/components/Item.js
@@ -3,11 +3,23 @@ import React from "react";
 import * as items from "./items/index.js";
 
 import styles from "./Item.module.css";
+import check from "./check.png";
 
-export default function Item({name, className}) {
+export default function Item({name, className, overlay}) {
     const id = name.toLowerCase().replace(/[^a-z]/g, "_");
 
+    let overlayElement = null;
+
+    switch(overlay) {
+        case "checked":
+            overlayElement = <img src={check} className={styles.overlayChecked} alt="" />;
+            break;
+    }
+
     return (
-        <img className={(className??"") + " " + styles.item} src={items[id]} alt={name} />
+        <span className={(className??"") + " " + styles.itemWrapper}>
+            <img className={styles.item} src={items[id]} alt={name} />
+            {overlayElement}
+        </span>
     )
 }
